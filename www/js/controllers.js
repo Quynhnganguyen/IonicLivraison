a8f824b1a04a2af8e88dd4dce60f79dc55f269a6
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-07-01 10:31:09 +0200

    list repeat auto

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('ListCtrl', function($scope){
+	$scope.livreurList = [
+		{nom: "Nguyen", etat: "En cours", tel: "0213456789", img:'abc.jpg'},
+		{nom: "Nguyen", etat: "En cours", tel: "0213456789", img:'abc.jpg'},
+		{nom: "Nguyen", etat: "En cours", tel: "0213456789", img:'abc.jpg'},
+		{nom: "Nguyen", etat: "En cours", tel: "0213456789", img:'abc.jpg'},
+		{nom: "Nguyen", etat: "En cours", tel: "0213456789", img:'abc.jpg'}
+		
+	]
 })
+.controller('MapCtrl', function($scope) {
+	$scope.map = {
+	    center: {
+	        latitude: 45,
+	        longitude: -73
+	    },
+	   
+	    zoom: 10
+	}
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+	$scope.marker = {
+		name: 'Hotel eden opera paris',
+		latitude: 48.856614,
+		longitude: 2.352221900000017700
+	};
 });
+
+
+
