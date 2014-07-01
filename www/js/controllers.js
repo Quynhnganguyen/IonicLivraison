c07345f7fe4d1346301361a19ae8c79537a39a5d
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-07-01 11:31:28 +0200

    marker debut

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,18 +13,42 @@ angular.module('starter.controllers', [])
 .controller('MapCtrl', function($scope) {
 	$scope.map = {
 	    center: {
-	        latitude: 45,
-	        longitude: -73
+	        latitude: 48.856614,
+	        longitude: 2.352221900000017700
 	    },
 	   
 	    zoom: 10
 	}
 
-	$scope.marker = {
-		name: 'Hotel eden opera paris',
-		latitude: 48.856614,
-		longitude: 2.352221900000017700
-	};
+
+  
+
+	$scope.myMarkers = [
+		{
+			name: 'Hotel eden opera paris',
+			latitude: 48.856614,
+			longitude: 2.352221900000017700
+		},
+		{
+			name: 'Mercure Paris Massy Gare TGV',
+			latitude: 48.7270438,
+			longitude: 2.2638262
+		},
+		{
+			name: 'Hôtel de Ville',
+			latitude: 48.8601,
+			longitude: 2.3507
+		},
+		{
+			name: 'Place de la Porte de Versailles',
+			latitude: 48.832514700000000000,
+			longitude: 2.287249599999995500
+		}
+
+	];
+
+  	$scope.markers = $scope.myMarkers;
+  	$scope.fit = true;
 });
 
 
